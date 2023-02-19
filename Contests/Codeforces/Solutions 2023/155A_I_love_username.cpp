5437dfd55ed404eca9682793030a4af66d942210
Josue Romero <josue.romero@uniminuto.edu.co>
2023-02-18 21:46:25 -0500

    ♻️ Update

--- Contests/Codeforces/Solutions 2023/155A_I_love_username.cpp
+++ Contests/Codeforces/Solutions 2023/155A_I_love_username.cpp
@@ -1,22 +1,27 @@
 /**
  *   author:  josuerom
- *   created: 18/02/23 13:50:35
+ *   created: 18/02/23 21:20:19
 **/
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
    ios::sync_with_stdio(false); cin.tie(0);
-   int N;
-   cin >> N;
-   int contests[N];
-   for (auto &e : contests) cin >> e;
-   if (N==1 || N==2) cout << (N==1 ? "0" : "1") << '\n';
-   else {
-      int ans = 1;
-      for (int i = 2; i < n; i++) {
-         if ()
+   int n;
+   cin >> n;
+   int a[1005];
+   for(int i = 0; i < n; i++) cin >> a[i];
+   int ans = 0, max = a[0], min = a[0];
+   for(int j = 1; j < n; j++) {
+      if(a[j] > max) {
+         max = a[j];
+         ans++;
+      }
+      if(a[j] < min) {
+         min = a[j];
+         ans++;
       }
    }
+   cout << ans;
    return 0;
 }
