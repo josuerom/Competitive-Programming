96ce1c7904e5737738dba5c630cf6bada76ac956
Josué Romero <79048209+josuerom@users.noreply.github.com>
2023-03-12 15:23:38 -0500

    Template1

--- Templates/template_josuerom_1.cpp
+++ Templates/template_josuerom_1.cpp
@@ -9,20 +9,19 @@ using namespace std;
 void dbg_out() { cout << endl; }
 template<typename Head, typename... Tail>
 void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
 #define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#define all(vec) vec.begin(), vec.end()
-#define rall(vec) vec.rbegin(), vec.rend()
-#define sz(vec) (int) vec.size()
+#define all(a) a.begin(), a.end()
+#define pb(a) push_back(a)
+#define sz(a) (int) a.size()
 #define ll long long
 #define endl '\n'
 
 int main() {
-	ios::sync_with_stdio(false); cin.tie(0);
-	int tt;
-	cin >> tt;
-	while (tt--) {
-		// the code here
-	}
-	return 0;
+	 ios::sync_with_stdio(false);
+	 cin.tie(0);
+	 int tt;
+	 cin >> tt;
+	 while (tt--) {
+	 }
+	 return 0;
 }
